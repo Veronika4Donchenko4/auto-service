d6816e6b54a2ceb1dbe69cc50bd8be7b6e14d8d9
Nika Donchenko <donchenko.veronika96@gmail.com>
2023-10-28 23:46:40 +0200

    input

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,33 @@
+import { useState } from 'react';
+
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+  };
   return (
     <>
       <h1>Login Form</h1>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <label>
           Username:
-          <input type="text" name="username" />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            name="username"
+          />
         </label>
         <label>
           Password:
-          <input type="password" name="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+          />
         </label>
         <button type="submit">Login</button>
       </form>
