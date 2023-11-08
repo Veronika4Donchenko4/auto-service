232b0a94621786fa37c1929c922a74361602d9de
Nika <donchenko.veronika96@gmail..com>
2023-11-08 22:17:16 +0100

    optimization of logic

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -4,7 +4,7 @@ import styles from './Todo.module.css';
 
 const Todo = ({ todo, deleteTodo, toggleTodo }) => {
   return (
-    <div className={styles.todo} >
+    <div className={`${styles.todo} ${todo.isCompleted ? styles.completedTodo : ''}`} >
       <RiTodoFill className={styles.todoIcon} />
       <div className={styles.todoText}>{todo.text}</div>
       <RiDeleteBin2Line className={styles.deleteIcon} onClick={() => deleteTodo(todo.id)}/>
